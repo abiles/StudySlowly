3c18477159f0f194ba775e09cc242fabbb36a694
abiles <abiles@naver.com>
2018-11-13 22:10:24 +0900

    class1일차, 코드를 찾아서 복사해 오기전

---
+++ studySlowly/studySlowly/class1.cpp
@@ -0,0 +1,18 @@
+// www.isocpp.org
+// www.cppreference.com  - c++ÀÇ Á¤¼®
+#include <iostream>
+
+class Hello
+{
+	std::string msg;
+public:
+	Hello ( std::string s ) : msg ( s ) {}
+
+	
+
+};
+
+int main ()
+{
+
+}
